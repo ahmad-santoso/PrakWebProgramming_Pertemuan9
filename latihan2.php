98551a757e0027d696e00529ad6d25a2a9f8d641
Ahmad Santoso <ahmadsantoso800@gmail.com>
2024-04-24 12:26:28 +0700

    finish project

---
+++ latihan2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+    $txt1 = "Learn PHP";
+    $txt2 = "UPN Veteran Jakarta";
+    $x = 5;
+    $y = 4;
+
+    echo "<h2>". $txt1 . "</h2>";
+    echo "Study PTP at ". $txt2 . "<br>";
+    echo $x + $y;
+    ?>
+</body>
+</html>
